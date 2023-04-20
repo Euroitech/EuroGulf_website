831c6933d69fa480fbf8de813100730f4fd93b72
Euroitech <130956980+Euroitech@users.noreply.github.com>
2023-04-20 11:20:12 +0530

    Delete admin directory

--- admin/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$hostname='creativetecin.ipagemysql.com';
-$user ='Creativetec';
-$password ='Ctech@123';
-$mysql_database ='eurogulfsafety';
-$db = mysql_connect($hostname, $user, $password,$mysql_database);
-mysql_select_db("eurogulfsafety", $db);
-?>
